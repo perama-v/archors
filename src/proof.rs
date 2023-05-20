95d0e7942d961bbe3045910d0e795f2be7ea2326
perama-v <83961755+perama-v@users.noreply.github.com>
2023-05-20 13:00:44 +1000

    make proof ambivalent to proof kind

--- src/proof.rs
+++ src/proof.rs
@@ -1,84 +1,142 @@
-use rlp::{self};
+//! For verifying a generic proof kind.
+use ethers::{types::Bytes, utils::keccak256};
+use hex::FromHexError;
+use rlp::{self, Rlp};
 use rlp_derive::{RlpDecodable, RlpEncodable};
 use serde::Deserialize;
 use thiserror::Error;
-use web3::{
-    signing::keccak256,
-    types::{Bytes, Proof, H256, U256},
+
+use crate::{
+    eip1186::Account,
+    node::{NodeError, NodeKind},
+    path::{NibblePath, PathError},
+    utils::{hex_encode, UtilsError},
 };
 
 #[derive(Debug, Error)]
 pub enum ProofError {
-    #[error("VerificationError {0}")]
-    VerificationError(String),
+    #[error("Branch node (non-terminal) has value, expected none")]
+    BranchNodeHasValue,
+    #[error("Parent nodes (rlp: {parent}) do not contain hash of rlp(child) (hash: {child}")]
+    ChildNotFound { parent: String, child: String },
     #[error("RLP decode error {0}")]
     DecodeError(#[from] rlp::DecoderError),
     #[error("Proof is empty")]
     EmptyProof,
-    #[error("Proof missing nonce field")]
-    NoNonce,
+    #[error("RLP decode error {0}")]
+    FromHexError(#[from] FromHexError),
+    #[error(
+        "Hash of node {computed} does not match the expected hash in the parent node {expected}"
+    )]
+    IncorrectHash { computed: String, expected: String },
+    #[error("Merkle Patricia Node to have max 17 (16 + 1) items, got {0}")]
+    InvalidNodeItemCount(usize),
+    #[error("Node (index = {node_index} error {source}")]
+    NodeError {
+        source: NodeError,
+        node_index: usize,
+    },
     #[error("Account proof node has no value. RLP: {0}")]
     NoNodeAccountValue(String),
     #[error("Storage proof node has no value. RLP: {0}")]
     NoNodeStorageValue(String),
-    #[error("Account in the proof did not match expected value")]
-    IncorrectAccount,
-    #[error("Storage value in the proof did not match expected value")]
-    IncorrectStorage,
-    #[error("Parent nodes (rlp: {parent}) do not contain hash of rlp(child) (hash: {child}")]
-    ChildNotFound { parent: String, child: String },
+    #[error("Path error {source} for node (index = {node_index} ")]
+    PathError {
+        source: PathError,
+        node_index: usize,
+    },
+    #[error("UtilsError {0}")]
+    UtilsError(#[from] UtilsError),
+    #[error("VerificationError {0}")]
+    VerificationError(String),
 }
 
-/// Verifies a single account proof with respect to a state roof. The
-/// proof is of the form returned by eth_getProof.
-pub(crate) fn verify_proof<T: AsRef<str>>(_account: T, proof: &Proof) -> Result<(), ProofError> {
-    // Account proof
+/// A proof for some data in a Merkle Patricia Tree, such as an account, or a storage value.
+#[derive(Default, Debug, Clone, PartialEq, Eq, Deserialize)]
+struct SingleProofPath {
+    /// Merkle PATRICIA trie proof for a key/value.
+    proof: Vec<Bytes>,
+    /// Trusted root that the proof anchors to.
+    root: [u8; 32],
+    /// Anticipated trie path to traverse for the proof.
+    path: [u8; 32],
+    /// Claimed value to be proven. E.g., RLP(account), or RLP(storage_value)
+    value: Vec<u8>,
+}
 
-    // TODO verify hash(top_level) == state_root.
+/// Holds information useful during a proof verification.
+pub struct Verifier {
+    data: SingleProofPath,
+}
 
-    let account_proof_final_level: &Bytes =
-        proof.account_proof.last().ok_or(ProofError::EmptyProof)?;
-    // Check account values correct.
-    let derived_account: Account = rlp_decode_final_account_element(&account_proof_final_level.0)?;
-    let claimed_account = Account {
-        nonce: proof.nonce,
-        balance: proof.balance,
-        storage_hash: proof.storage_hash,
-        code_hash: proof.code_hash,
-    };
-    if !derived_account.eq(&claimed_account) {
-        return Err(ProofError::IncorrectAccount);
-    }
-    // Check storage proof structure.
-    verify_parents_contain_children(&proof.account_proof)?;
-
-    // Storage proofs for this account
-    for storage_proof in &proof.storage_proof {
-        let storage_proof_final_level: &Bytes =
-            storage_proof.proof.last().ok_or(ProofError::EmptyProof)?;
-        // Check storage value is correct.
-        let derived_storage: Storage =
-            rlp_decode_final_storage_element(&storage_proof_final_level.0)?;
-        let claimed_storage = Storage(storage_proof.value);
-        if !derived_storage.eq(&claimed_storage) {
-            return Err(ProofError::IncorrectStorage);
+impl Verifier {
+    pub fn new_single_proof(
+        proof: Vec<Bytes>,
+        root: [u8; 32],
+        path: [u8; 32],
+        value: Vec<u8>,
+    ) -> Self {
+        Verifier {
+            data: SingleProofPath {
+                proof,
+                root,
+                path,
+                value,
+            },
         }
-        // Check storage proof structure.
-        verify_parents_contain_children(&storage_proof.proof)?;
+    }
+    pub fn verify(&self) -> Result<(), ProofError> {
+        // Traverse path
+        let total_nodes = self.data.proof.len();
+        if total_nodes == 0 {
+            return Err(ProofError::EmptyProof);
+        }
+        let mut traversal = NibblePath::init(&self.data.path);
+        let mut parent_hash = self.data.root;
+
+        for (node_index, rlp_node) in self.data.proof.iter().enumerate() {
+            node_hash_correct(&rlp_node.0, parent_hash)?;
+
+            let node: Vec<Vec<u8>> = rlp::decode_list(&rlp_node.0);
+            let status = NodeKind::deduce(node_index, total_nodes, node.len())
+                .map_err(|source| ProofError::NodeError { source, node_index })?;
+            let path_nibble = traversal
+                .visit_path_nibble()
+                .map_err(|source| ProofError::PathError { source, node_index })?;
+
+            let proof_type = status
+                .check_contents(node, path_nibble.into(), &mut traversal, &mut parent_hash)
+                .map_err(|source| ProofError::NodeError { source, node_index })?;
+            match proof_type {
+                //todo!("handle check of leaf bytes / exclusion proof values.");
+                ProofType::Inclusion(_) => todo!(),
+                ProofType::Exclusion => todo!(),
+                ProofType::Pending => {}
+            };
+        }
+        // If end up with a value ensure it equals self.data.value
+        Ok(())
+    }
+}
+
+/// Checks that the hash of one node is correct.
+fn node_hash_correct(rlp_node: &[u8], parent_hash: [u8; 32]) -> Result<(), ProofError> {
+    let computed_hash = keccak256(rlp_node);
+    if !computed_hash.eq(&parent_hash) {
+        let computed = hex_encode(computed_hash);
+        let expected = hex_encode(parent_hash);
+        return Err(ProofError::IncorrectHash { computed, expected });
     }
     Ok(())
 }
 
-#[derive(Default, Debug, Clone, PartialEq, Eq, Deserialize, RlpEncodable, RlpDecodable)]
-struct Account {
-    nonce: U256,
-    balance: U256,
-    storage_hash: H256,
-    code_hash: H256,
+pub enum ProofType {
+    /// Inclusion proof with leaf bytes for verification.
+    Inclusion(Vec<u8>),
+    Exclusion,
+    /// Not yet finished processing the proof.
+    Pending,
 }
-
-#[derive(Default, Debug, Clone, PartialEq, Eq, Deserialize, RlpEncodable, RlpDecodable)]
-struct Storage(U256);
 
 /// A merkle patricia trie node at any level/height of an account proof.
 #[derive(Default, Debug, Clone, PartialEq, Eq, Deserialize, RlpEncodable, RlpDecodable)]
@@ -119,6 +177,7 @@ impl From<[u8; 32]> for Item {
         Self(value.to_vec())
     }
 }
+
 /// Checks that the merkle proof consists of hashes linking every level to the one above.
 ///
 /// A proof consists of a list of RLP-encoded data: (A, B, ... C)
@@ -133,6 +192,9 @@ fn verify_parents_contain_children(nodes: &[Bytes]) -> Result<(), ProofError> {
     // Walk from leaves to root.
     for node_bytes in nodes.iter().rev().skip(1) {
         let node: Vec<Vec<u8>> = rlp::decode_list(&node_bytes.0);
+        if node.len() > 17 {
+            return Err(ProofError::InvalidNodeItemCount(node.len()));
+        }
         if !node.contains(&hash_to_check.into()) {
             return {
                 let child = hex::encode(hash_to_check);
@@ -146,66 +208,23 @@ fn verify_parents_contain_children(nodes: &[Bytes]) -> Result<(), ProofError> {
     Ok(())
 }
 
-/// Decodes the final element of an storage proof and returns an storage object.
-///
-/// The element comprises: rlp(nodes, rlp(storage_object))
-///
-/// Where storage_object contains: nonce, balance, storage_hash, code_hash.
-/// Nodes at this level are discarded as they are not used to evaluate the proof.
-fn rlp_decode_final_storage_element(proof_leaf_rlp: &[u8]) -> Result<Storage, ProofError> {
-    let rlp: Vec<Vec<u8>> = rlp::decode_list(proof_leaf_rlp);
-    let storage_value = rlp
-        .last()
-        .ok_or(ProofError::NoNodeStorageValue(hex::encode(proof_leaf_rlp)))?;
-    let val = U256::from_big_endian(storage_value);
-    Ok(Storage(val))
-}
-
-/// Decodes the final element of an account proof and returns an account object.
-///
-/// The element comprises: rlp(nodes, rlp(account_object))
-///
-/// Where account_object contains: nonce, balance, storage_hash, code_hash.
-/// Nodes at this level are discarded as they are not used to evaluate the proof.
-fn rlp_decode_final_account_element(proof_leaf_rlp: &[u8]) -> Result<Account, ProofError> {
-    let rlp: Vec<Vec<u8>> = rlp::decode_list(proof_leaf_rlp);
-    let account_rlp = rlp
-        .last()
-        .ok_or(ProofError::NoNodeAccountValue(hex::encode(proof_leaf_rlp)))?;
-    let account: Account = rlp::decode(account_rlp)?;
-    Ok(account)
-}
-
 #[cfg(test)]
 mod tests {
     use std::{fs::File, io::BufReader, str::FromStr};
 
+    use ethers::types::{EIP1186ProofResponse, H256};
     use rlp::Rlp;
 
     use super::*;
-    // A 17-item merkle patricia trie node.
-    const BRANCH_NODE: &str = "f90151a0bf5e7a6355d2aae16870034397bcb78fb7f3677302857c4e3f0f11b2ad183ddaa0441a130e5b3344a0c6d4e01e69cdd8c3d54c9427c22df1c21e823bd5238bcedc80a0de4a8735f0afe745a73341f09b2641b136c4c6ceb33a4c04f868b8c0ae0c572da0616b1953ab56f21db0e3e0a8f04422bbdce75bd530e049560426deb7548c9324a0df7498a408a3cb6f416a60eb97bc61cdd31f9f9c1e3d9f2e131c476cca1a64aaa0b4b838d595815f1af27bc520f9054bbe7b8f1ae901d58ceba455a93a02b38fe3a088c2648a34b76ec09c67666bf1b2ff917c97a960dbebd2c8d56ec2b89c5f5d7ba080f002d80dc9f4e682660964f02c4f70fdfb5aeeee5f5651fca75c06f810c37980a0f6d68b8a203434af63aefd6acbce4e627b80e03c11d9c64334d48655f842ee24a02991191455c868799650d6cd4009a21443c9ac2aebedb76d55d9a01811d59a9c8080808080";
 
     // A 2-item merkle patricia trie node. RLP structure: list[integer, list[account]]
     const ACCOUNT_LEAF: &str = "f8669d33269ec9b8f075a4723d27c611ac1c52a464f3516b25e0105a0d1c2210b846f8440180a03836d7e3afb674e5180b7564e096f6f3e30308878a443fe59012ced093544b7fa02cfdfbdd943ec0153ed07b97f03eb765dc11cc79c6f750effcc2d126f93c4b31";
 
-    // A 2-item merkle patricia trie node. RLP structure: list[string, ..., list[storage]]
-    // This proof is for storage key = 0, value = 0.
-    const STORAGE_LEAF: &str =
-    "f871a0e4050339952e88a1d403d7078148abf3af96d8a2fdb175cf12244b721962fe4280808080808080a0cd71d6a12adb2cef5dba915f9cd9490173c5db30ea44a1aee026d8e0ea2fd27f80a059267a0b25d180d3cae2274c50da7b7da0ddddfd435671181e9dc2f7ba8cca7f808080808080";
-
-    fn load_proof() -> Proof {
+    fn load_proof() -> EIP1186ProofResponse {
         // data src: https://github.com/gakonst/ethers-rs/blob/master/ethers-core/testdata/proof.json
-        let file = File::open("data/test_proof.json").expect("no proof found");
+        let file = File::open("data/test_proof_2.json").expect("no proof found");
         let reader = BufReader::new(&file);
         serde_json::from_reader(reader).expect("could not parse proof")
-    }
-
-    #[test]
-    fn test_verify_proof() {
-        let account_proof = load_proof();
-        let address: &str = "0x7ae1d57b58fa6411f32948314badd83583ee0e8c";
-        verify_proof(address, &account_proof).expect("could not verify proof");
     }
 
     #[test]
@@ -223,149 +242,39 @@ mod tests {
     }
 
     #[test]
-    fn rlp_decode_account_proof_leaf() {
+    fn test_node_hash() {
         // RLP-encoded account leaf
-        let data_bytes = hex::decode(ACCOUNT_LEAF).unwrap();
-        let account = rlp_decode_final_account_element(&data_bytes).unwrap();
+        let node_rlp = hex::decode(ACCOUNT_LEAF).unwrap();
+        let hash = hex::decode("de4a8735f0afe745a73341f09b2641b136c4c6ceb33a4c04f868b8c0ae0c572d")
+            .unwrap();
+        let expected_hash = H256::from_slice(&hash).0;
+        node_hash_correct(&node_rlp, expected_hash).unwrap();
+    }
+
+    #[test]
+    fn keccak() {
+        let hex_input = "";
+        let bytes = hex::decode(hex_input).unwrap();
+        let hash = hex::encode(keccak256(&bytes));
         assert_eq!(
-            account,
-            Account {
-                nonce: 1.into(),
-                balance: 0.into(),
-                storage_hash: H256::from_str(
-                    "0x3836d7e3afb674e5180b7564e096f6f3e30308878a443fe59012ced093544b7f"
-                )
-                .unwrap(),
-                code_hash: H256::from_str(
-                    "0x2cfdfbdd943ec0153ed07b97f03eb765dc11cc79c6f750effcc2d126f93c4b31"
-                )
-                .unwrap()
-            },
+            hash,
+            "c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"
         );
-    }
 
-    #[test]
-    fn rlp_decode_storage_proof_leaf_val_zero() {
-        // RLP-encoded storage leaf
-        let data_bytes = hex::decode(STORAGE_LEAF).unwrap();
-        let storage = rlp_decode_final_storage_element(&data_bytes).unwrap();
-        assert_eq!(storage, Storage::default());
-    }
+        let hex_input = "00";
+        let bytes = hex::decode(hex_input).unwrap();
+        let hash = hex::encode(keccak256(&bytes));
+        assert_eq!(
+            hash,
+            "bc36789e7a1e281436464229828f817d6612f7b477d66591ff96a9e064bcc98a"
+        );
 
-    #[test]
-    fn rlp_decode_storage_proof_leaf_val_nonzero() {
-        // RLP-encoded storage leaf
-        let data_bytes = hex::decode("ed9f208376e489f0656e4ae2d3a7f1b2fc108b42db04095810f535cc0ab222a6498c8b0422ca8b0a00a425000000").unwrap();
-        let storage = rlp_decode_final_storage_element(&data_bytes).unwrap();
-        let expected = U256::from_str("0x8b0422ca8b0a00a425000000").unwrap();
-        assert_eq!(storage, Storage(expected));
-    }
-
-    #[test]
-    fn rlp_basic() {
-        let data = vec![0xc8, 0x83, b'c', b'a', b't', 0x83, b'd', b'o', b'g'];
-        let rlp = Rlp::new(&data);
-        let dog = rlp.at(1).unwrap().data().unwrap();
-        assert_eq!(dog, &[b'd', b'o', b'g']);
-    }
-
-    #[test]
-    fn rlp_short_list() {
-        let data = vec![0xc8, 0x83, b'c', b'a', b't', 0x83, b'd', b'o', b'g'];
-        let list: Vec<String> = rlp::decode_list(&data);
-        assert_eq!(2, list.len());
-        assert_eq!(list.get(1).unwrap(), &"dog".to_string());
-    }
-
-    #[derive(RlpDecodable)]
-    struct Animals {
-        first: String,
-        second: String,
-    }
-
-    #[derive(RlpDecodable)]
-    struct AnimalsInvalid {
-        first: Vec<u8>,
-        second: String,
-    }
-
-    #[derive(RlpDecodable)]
-    struct AnimalsGroupedStruct {
-        together: Vec<String>,
-    }
-
-    #[derive(RlpDecodable)]
-    struct AnimalsGroupedTupleStruct(Vec<String>);
-
-    #[test]
-    fn rlp_struct_short_list() {
-        let data = vec![0xc8, 0x83, b'c', b'a', b't', 0x83, b'd', b'o', b'g'];
-        let animals: Animals = rlp::decode(&data).unwrap();
-        assert_eq!(animals.first, "cat".to_string());
-        assert_eq!(animals.second, "dog".to_string());
-
-        let animals_invalid: AnimalsInvalid = rlp::decode(&data).unwrap();
-        // First is a string, so is to be ignored as the struct expects bytes.
-        assert!(animals_invalid.first.is_empty());
-        assert_eq!(animals_invalid.second, "dog".to_string());
-
-        let animals_grouped_struct: AnimalsGroupedStruct = rlp::decode(&data).unwrap();
-        // Neither item is detected when grouped.
-        assert_eq!(animals_grouped_struct.together.len(), 0);
-
-        let animals_grouped_tuple_struct: AnimalsGroupedTupleStruct = rlp::decode(&data).unwrap();
-        // Neither item is detected when grouped as tuple.
-        assert_eq!(animals_grouped_tuple_struct.0.len(), 0);
-    }
-
-    #[test]
-    fn rlp_long_list_basic() {
-        let data_bytes = hex::decode(BRANCH_NODE).unwrap();
-        let rlp = Rlp::new(&data_bytes);
-        let known_first_item = "bf5e7a6355d2aae16870034397bcb78fb7f3677302857c4e3f0f11b2ad183dda";
-        let known_last_item = "";
-
-        // As item
-        assert_eq!(rlp.item_count().unwrap(), 17);
-        let first_item = rlp.at(0).unwrap().data().unwrap();
-        assert_eq!(first_item, hex::decode(known_first_item).unwrap());
-        let last_item = rlp.at(16).unwrap().data().unwrap();
-        assert_eq!(last_item, hex::decode(known_last_item).unwrap());
-
-        // As list
-        let node_list: Vec<Vec<u8>> = rlp.as_list().unwrap();
-        let first_item = node_list.get(0).unwrap();
-        assert_eq!(first_item, &hex::decode(known_first_item).unwrap());
-    }
-
-    #[test]
-    fn rlp_long_list_basic_heterogeneous() {
-        let data_bytes = hex::decode(ACCOUNT_LEAF).unwrap();
-        let rlp = Rlp::new(&data_bytes);
-        let known_first_item = "33269ec9b8f075a4723d27c611ac1c52a464f3516b25e0105a0d1c2210";
-        // Account (list of items).
-        let known_last_item = "f8440180a03836d7e3afb674e5180b7564e096f6f3e30308878a443fe59012ced093544b7fa02cfdfbdd943ec0153ed07b97f03eb765dc11cc79c6f750effcc2d126f93c4b31";
-
-        // As item
-        assert_eq!(rlp.item_count().unwrap(), 2);
-        let first_item = rlp.at(0).unwrap().data().unwrap();
-        assert_eq!(first_item, hex::decode(known_first_item).unwrap());
-        let last_item = rlp.at(1).unwrap().data().unwrap();
-        assert_eq!(last_item, hex::decode(known_last_item).unwrap());
-
-        // As list
-        let node_list: Vec<Vec<u8>> = rlp.as_list().unwrap();
-        let first_item = node_list.get(0).unwrap();
-        assert_eq!(first_item, &hex::decode(known_first_item).unwrap());
-
-        // Second list
-        let inner_rlp = Rlp::new(last_item);
-        assert!(inner_rlp.is_list());
-        let account: Account = rlp::decode(last_item).unwrap();
-        assert_eq!(account.nonce, 1.into());
-        let known_code_hash =
-            hex::decode("2cfdfbdd943ec0153ed07b97f03eb765dc11cc79c6f750effcc2d126f93c4b31")
-                .unwrap();
-        assert_eq!(account.code_hash.as_bytes(), known_code_hash);
+        let hex_input = "01";
+        let bytes = hex::decode(hex_input).unwrap();
+        let hash = hex::encode(keccak256(&bytes));
+        assert_eq!(
+            hash,
+            "5fe7f977e71dba2ea1a68e21057beebb9be2ac30c6410aa38d4f3fbe41dcffd2"
+        );
     }
 }
