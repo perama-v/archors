49af21d611576d2e34a8873dbaf19ed6ba3f6eb1
perama-v <83961755+perama-v@users.noreply.github.com>
2023-05-22 15:12:53 +1000

    fix: visit path for nonterminal branch

--- src/node.rs
+++ src/node.rs
@@ -7,14 +7,14 @@ use crate::{
     proof::ProofType,
 };
 
-#[derive(Debug, Error)]
+#[derive(Debug, Error, Eq, PartialEq)]
 pub enum NodeError {
-    #[error("Branch node (non-terminal) has value, expected none")]
+    #[error("Branch node (non-terminal) expected to be empty")]
     BranchNodeHasValue,
+    #[error("Branch node (non-terminal) has less than 17 items")]
+    BranchNodeHasNoValue,
     #[error("Branch node item expected to be 32 bytes")]
     BranchNodeItemInvalidLength,
-    #[error("Terminal extension/leaf node is exclusion proof, but has a non-empty final item")]
-    ExclusionProofNodeHasValue,
     #[error("Extension node (non-terminal) has no path extension")]
     ExtensionNodeHasValue,
     #[error("Extension node item expected to be 32 bytes")]
@@ -41,17 +41,18 @@ pub enum NodeError {
     TerminalExtensionHasFullPath,
     #[error("Terminal extension/leaf node expected to have a final path, found none")]
     TerminalExtensionOrLeafHasNoPath,
-    #[error("Terminal leaf node has no value")]
-    TerminalLeafHasNoValue,
-    #[error("Terminal leaf node expected to have a final path, found none")]
-    TerminalLeafHasNoPath,
-    #[error("Terminal leaf node must complete the 32 byte path")]
-    TerminalLeafHasIncompletePath,
+    #[error("Leaf node has no value")]
+    LeafHasNoValue,
+    #[error("Leaf node expected to have a final path, found none")]
+    LeafHasNoPath,
+    #[error("Leaf node must complete the 32 byte path")]
+    LeafHasIncompletePath,
     #[error("VerificationError {0}")]
     VerificationError(String),
 }
 
 /// Description of node in a merkle proof.
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub enum NodeKind {
     // 17 items, not at end
     Branch,
@@ -95,32 +96,41 @@ impl NodeKind {
         };
         Ok(kind)
     }
-    /// Checks that a particular item of a node is valid.
+    /// Visits the relevant part of a node and checks values are as expected.
+    ///
+    /// This may involve progressing the path traversal or making a determination about
+    /// the proof kind if the final node is reached.
     ///
     /// If an intermediate node, returns the hash that is now the parent for the next node.
     /// A node is a vector of items (bytes representing one of path/hash/rlp_value)
-    pub fn check_contents(
+    pub fn traverse_node(
         &self,
         node: Vec<Vec<u8>>,
-        item_index: usize,
         traversal: &mut NibblePath,
-        parent_root: &mut [u8; 32],
+        parent_root_to_update: &mut [u8; 32],
     ) -> Result<ProofType, NodeError> {
         match self {
             NodeKind::Branch => {
-                // Assert value item is None (not terminal).
-                if node.get(17).is_some() {
+                // Assert value item is empty (not terminal).
+                let final_item = node.get(16).ok_or_else(||
+                    NodeError::BranchNodeHasNoValue)?;
+
+                if !final_item.is_empty() {
                     return Err(NodeError::BranchNodeHasValue);
                 }
                 // Send back a new parent node
+                let path_nibble = traversal
+                    .visit_path_nibble()
+                    .map_err(NodeError::PathError)?;
+
                 let item = node
-                    .get(item_index)
+                    .get(path_nibble as usize)
                     .ok_or_else(|| NodeError::NoNodeToTraverse)?;
                 if item.len() != 32 {
                     return Err(NodeError::BranchNodeItemInvalidLength);
                 }
                 let next_root: [u8; 32] = H256::from_slice(&item).into();
-                *parent_root = next_root;
+                *parent_root_to_update = next_root;
                 Ok(ProofType::Pending)
             }
             NodeKind::Extension => {
@@ -141,7 +151,7 @@ impl NodeKind {
                     return Err(NodeError::ExtensionNextNodeInvalidLength);
                 }
                 let next_root: [u8; 32] = H256::from_slice(&item).into();
-                *parent_root = next_root;
+                *parent_root_to_update = next_root;
                 Ok(ProofType::Pending)
             }
             NodeKind::TerminalBranch => {
@@ -149,7 +159,7 @@ impl NodeKind {
 
                 // Assert value item is Some(empty_list) (terminal).
                 let value = node
-                    .get(17)
+                    .get(16)
                     .ok_or_else(|| NodeError::TerminalBranchNodeHasNoValue)?;
                 if value.is_empty() {
                     return Ok(ProofType::Exclusion);
@@ -185,15 +195,11 @@ impl NodeKind {
                 }
             }
             NodeKind::Leaf => {
-                let first_item = node
-                    .get(0)
-                    .ok_or_else(|| NodeError::TerminalLeafHasNoPath)?;
-                let second_item = node
-                    .get(1)
-                    .ok_or_else(|| NodeError::TerminalLeafHasNoValue)?;
+                let first_item = node.get(0).ok_or_else(|| NodeError::LeafHasNoPath)?;
+                let second_item = node.get(1).ok_or_else(|| NodeError::LeafHasNoValue)?;
                 match traversal.match_or_mismatch(first_item)? {
                     PathNature::SubPathMatches | PathNature::SubPathDivergent => {
-                        Err(NodeError::TerminalLeafHasIncompletePath)
+                        Err(NodeError::LeafHasIncompletePath)
                     }
                     PathNature::FullPathMatches => {
                         if second_item.is_empty() {
@@ -211,9 +217,38 @@ impl NodeKind {
 }
 
 mod test {
+    use crate::utils::{hex_decode, hex_encode};
+
+    use super::*;
+
+    fn rlp_decode_node(node: &str) -> Vec<Vec<u8>> {
+        let bytes = hex_decode(node).unwrap();
+        rlp::decode_list(&bytes)
+    }
+
+    // Checks the final node in an account inclusion proof.
     #[test]
     fn test_inclusion_leaf_for_nonzero_value() {
-        todo!()
+        // src: account proof from ./data/test_proof_1.json
+        // account address: 0xaa00000000000000000000000000000000000000
+        // path (keccak(address)): 0x735649db80be637d281db0cc5896b0ff9869d08379a80fdc38dd073bba633949
+        // Two nodes:
+        // - First node is extension node, item index 7 is traversed.
+        // - Second node is leaf node with remaining full path, hence inclusion proof.
+        let node = rlp_decode_node("0xf869a0335649db80be637d281db0cc5896b0ff9869d08379a80fdc38dd073bba633949b846f8440101a08afc95b7d18a226944b9c2070b6bda1c3a36afcc3730429d47579c94b9fe5850a0ce92c756baff35fa740c3557c1a971fd24d2d35b7c8e067880d50cd86bb0bc99");
+        let node_kind = NodeKind::deduce(1, 2, 2, &node).unwrap();
+        assert_eq!(node_kind, NodeKind::Leaf);
+
+        let mut traversal = NibblePath::init(
+            &hex_decode("0x735649db80be637d281db0cc5896b0ff9869d08379a80fdc38dd073bba633949")
+                .unwrap(),
+        );
+        assert_eq!(traversal.visit_path_nibble().unwrap(), 0x7);
+        let mut parent_root_to_update = [0u8; 32];
+        node_kind
+            .traverse_node(node, &mut traversal, &mut parent_root_to_update)
+            .unwrap();
+        todo!("add another check")
     }
     #[test]
     fn test_inclusion_leaf_for_zero_value() {
