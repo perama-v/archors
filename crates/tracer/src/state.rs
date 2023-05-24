c061b27007252cd78b8722a199cc721d2bd20b8a
perama-v <83961755+perama-v@users.noreply.github.com>
2023-05-24 23:41:09 +1000

    add revm in-memory db

--- crates/tracer/src/state.rs
+++ crates/tracer/src/state.rs
@@ -1,24 +1,81 @@
 //! For representing state for an historical block.
 
-use archors_inventory::types::BlockProofs;
+use archors_inventory::utils::hex_encode;
+use ethers::types::EIP1186ProofResponse;
+use revm::{
+    db::{CacheDB, EmptyDB},
+    primitives::{AccountInfo, Bytecode, Bytes, HashMap, B160},
+};
 
-/// Properties that state representation must have.
-///
-/// This is useful if there is a more efficient way to represent
-/// state compared to unmodified EIP-1186 response, which has duplicate
-/// data.
-trait BlockState {
-    /// Get a value for a storage key.
-    fn read_key() {}
-    /// Modify a value for a storage key.
-    fn write_key() {}
+use thiserror::Error;
+
+/// An error with tracing a block
+#[derive(Debug, Error, Eq, PartialEq)]
+pub enum StateError {
+    #[error("Unable to get account state proof for address")]
+    NoProofForAddress(String),
+    #[error("Unable to get account code for address")]
+    NoCodeForAddress(String),
 }
 
+/// A basic map of accounts to proofs. Contains duplicated data and
+/// does not contain contract code.
+pub struct BlockProofsBasic {
+    /// Map of codehash -> code
+    pub code: HashMap<B160, Bytes>,
+    /// Map of account -> proof
+    pub proofs: HashMap<B160, EIP1186ProofResponse>,
+}
 
-impl BlockState for BlockProofs {
-    fn read_key() {
-        println!();
+/// Behaviour that any proof-based format must provide to be convertible into
+/// a reth DB.
+pub trait CompleteAccounts {
+    /// Gets account information in a format that can be inserted into a
+    /// revm db.
+    fn to_account_info(&self, address: &B160) -> Result<AccountInfo, StateError>;
+
+    fn addresses(&self) -> Vec<B160>;
+}
+
+impl CompleteAccounts for BlockProofsBasic {
+    fn to_account_info(&self, address: &B160) -> Result<AccountInfo, StateError> {
+        let account = self
+            .proofs
+            .get(address)
+            .ok_or_else(|| StateError::NoProofForAddress(hex_encode(address)))?;
+        let code: Option<Bytecode> = self
+            .code
+            .get(address)
+            .map(|bytes| Bytecode::new_raw(bytes.clone()));
+
+        let info = AccountInfo {
+            balance: account.balance.into(),
+            nonce: account.nonce.as_u64(),
+            code_hash: account.code_hash.0.into(),
+            code: code.into(),
+        };
+        Ok(info)
+    }
+    fn addresses(&self) -> Vec<B160> {
+        self.proofs
+            .iter()
+            .map(|(address, _proof)| *address)
+            .collect()
+    }
+}
+
+/// Inserts state from a collection of EIP-1186 proof into an in-memory DB.
+/// The DB can then be used to trace a block, recording state changes as they occur.
+pub fn build_state_from_proofs<T>(block_proofs: T) -> Result<CacheDB<EmptyDB>, StateError>
+where
+    T: CompleteAccounts,
+{
+    let mut db = CacheDB::new(EmptyDB::default());
+
+    for address in block_proofs.addresses() {
+        let info = block_proofs.to_account_info(&address)?;
+        db.insert_account_info(address, info);
     }
 
-    fn write_key() {}
+    Ok(db)
 }
--- crates/tracer/src/trace.rs
+++ crates/tracer/src/trace.rs
@@ -1,7 +1,14 @@
 //! For executing a block using state.
 
-use archors_inventory::types::BlockProofs;
+use std::str::FromStr;
+
+use archors_inventory::{types::BlockProofs, utils::hex_decode};
 use ethers::types::{Block, Transaction};
+use revm::{
+    db::{CacheDB, EmptyDB},
+    primitives::{AccountInfo, Bytes, TransactTo, B160, U256},
+    EVM,
+};
 use thiserror::Error;
 
 /// An error with tracing a block
@@ -11,7 +18,76 @@ pub enum TraceError {
     Todo,
 }
 
-pub fn trace_block(block: Block<Transaction>, state: BlockProofs) -> Result<(), TraceError> {
-    todo!();
+pub fn trace_block(block: Block<Transaction>, state_db: &BlockProofs) -> Result<(), TraceError> {
+    // For all important states, load into db.
+    let address = B160::default();
+    let account = AccountInfo::default();
+    let mut cache_db = CacheDB::new(EmptyDB::default());
+    cache_db.insert_account_info(address, account);
+
+    let slot = U256::default();
+    let value = U256::default();
+    cache_db
+        .insert_account_storage(address, slot, value)
+        .unwrap();
+
+    let mut evm = EVM::new();
+    // insert the db
+    evm.database(cache_db);
+
+    for transaction in block.transactions {}
+    // change that to whatever caller you want to be
+    evm.env.tx.caller = B160::from_str("0x0000000000000000000000000000000000000000").unwrap();
+    // account you want to transact with
+    evm.env.tx.transact_to = TransactTo::Call(B160::default());
+    // calldata formed via abigen
+    evm.env.tx.data = Bytes::from(hex_decode(&"0xabcd1234").unwrap());
+    // transaction value in wei
+    evm.env.tx.value = U256::from(0);
+    evm.transact_commit().unwrap();
+
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use std::collections::HashMap;
+
+    use archors_inventory::types::BlockProofs;
+    use revm::db::{CacheDB, DatabaseRef, EmptyDB};
+
+    use super::*;
+    #[test]
+    fn test_trace_block() {
+        let block: Block<Transaction> = Block::default();
+        let state = BlockProofs {
+            proofs: HashMap::default(),
+        };
+        trace_block(block, &state).unwrap()
+    }
+
+    #[test]
+    pub fn test_replace_account_storage() {
+        let account = 42.into();
+        let nonce = 42;
+        let mut init_state = CacheDB::new(EmptyDB::default());
+        init_state.insert_account_info(
+            account,
+            AccountInfo {
+                nonce,
+                ..Default::default()
+            },
+        );
+
+        let (key0, value0) = (U256::from(123), U256::from(456));
+        let (key1, value1) = (U256::from(789), U256::from(999));
+        let _ = init_state.insert_account_storage(account, key0, value0);
+
+        let mut new_state = CacheDB::new(init_state);
+        let _ = new_state.replace_account_storage(account, [(key1, value1)].into());
+
+        assert_eq!(new_state.basic(account).unwrap().unwrap().nonce, nonce);
+        assert_eq!(new_state.storage(account, key0), Ok(U256::ZERO));
+        assert_eq!(new_state.storage(account, key1), Ok(value1));
+    }
 }
