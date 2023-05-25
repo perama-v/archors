276e0afaaf44b456df94996fdda21913405cf8ff
perama-v <83961755+perama-v@users.noreply.github.com>
2023-05-25 17:25:06 +1000

    add evm state setup

--- crates/tracer/src/state.rs
+++ crates/tracer/src/state.rs
@@ -1,52 +1,65 @@
 //! For representing state for an historical block.
 
-use archors_inventory::utils::hex_encode;
-use ethers::types::EIP1186ProofResponse;
+use std::collections::HashMap;
+
+use ethers::types::{EIP1186ProofResponse, H160, H256};
 use revm::{
     db::{CacheDB, EmptyDB},
-    primitives::{AccountInfo, Bytecode, Bytes, HashMap, B160},
+    primitives::{AccountInfo, Bytecode, Bytes, HashMap as rHashMap, B160, U256},
 };
 
 use thiserror::Error;
 
+use crate::utils::{eh256_to_ru256, eu256_to_ru256, hex_encode, UtilsError};
+
 /// An error with tracing a block
-#[derive(Debug, Error, Eq, PartialEq)]
+#[derive(Debug, Error, PartialEq)]
 pub enum StateError {
     #[error("Unable to get account state proof for address")]
     NoProofForAddress(String),
     #[error("Unable to get account code for address")]
     NoCodeForAddress(String),
+    #[error("UtilsError {0}")]
+    UtilsError(#[from] UtilsError),
+    #[error("Could not initialise storage for account {address}, error {error}")]
+    AccountStorageInit { error: String, address: String },
 }
 
-/// A basic map of accounts to proofs. Contains duplicated data and
-/// does not contain contract code.
+/// A basic map of accounts to proofs.
+///
+/// Basic referring to the presence of duplicated trie nodes throughout the data.
 pub struct BlockProofsBasic {
-    /// Map of codehash -> code
-    pub code: HashMap<B160, Bytes>,
     /// Map of account -> proof
-    pub proofs: HashMap<B160, EIP1186ProofResponse>,
+    pub proofs: HashMap<H160, EIP1186ProofResponse>,
+    /// Map of codehash -> code
+    pub code: HashMap<H256, Vec<u8>>,
 }
 
 /// Behaviour that any proof-based format must provide to be convertible into
 /// a reth DB.
+///
+/// Returned types are revm-based.
 pub trait CompleteAccounts {
     /// Gets account information in a format that can be inserted into a
     /// revm db.
-    fn to_account_info(&self, address: &B160) -> Result<AccountInfo, StateError>;
-
+    fn get_account_info(&self, address: &B160) -> Result<AccountInfo, StateError>;
+    /// Gets all the addresses.
     fn addresses(&self) -> Vec<B160>;
+    /// Gets the storage key-val pairs for the account of the address.
+    fn get_account_storage(&self, address: &B160) -> Result<rHashMap<U256, U256>, StateError>;
 }
 
 impl CompleteAccounts for BlockProofsBasic {
-    fn to_account_info(&self, address: &B160) -> Result<AccountInfo, StateError> {
+    fn get_account_info(&self, address: &B160) -> Result<AccountInfo, StateError> {
         let account = self
             .proofs
-            .get(address)
+            .get(&address.0.into())
             .ok_or_else(|| StateError::NoProofForAddress(hex_encode(address)))?;
-        let code: Option<Bytecode> = self
-            .code
-            .get(address)
-            .map(|bytes| Bytecode::new_raw(bytes.clone()));
+
+        let code: Option<Bytecode> = self.code.get(&account.code_hash).map(|data| {
+            let revm_bytes = Bytes::copy_from_slice(data);
+            Bytecode::new_raw(revm_bytes)
+        });
 
         let info = AccountInfo {
             balance: account.balance.into(),
@@ -56,26 +69,51 @@ impl CompleteAccounts for BlockProofsBasic {
         };
         Ok(info)
     }
+    fn get_account_storage(&self, address: &B160) -> Result<rHashMap<U256, U256>, StateError> {
+        let account = self
+            .proofs
+            .get(&address.0.into())
+            .ok_or_else(|| StateError::NoProofForAddress(hex_encode(address)))?;
+
+        // Storage key-val pairs for the account.
+        let mut storage: rHashMap<U256, U256> = rHashMap::new();
+
+        for storage_data in &account.storage_proof {
+            // U256 ethers -> U256 revm
+            let key = eh256_to_ru256(storage_data.key)?;
+            let value = eu256_to_ru256(storage_data.value)?;
+
+            storage.insert(key, value);
+        }
+
+        Ok(storage)
+    }
     fn addresses(&self) -> Vec<B160> {
         self.proofs
             .iter()
-            .map(|(address, _proof)| *address)
+            .map(|(address, _proof)| B160::from(address.0))
             .collect()
     }
 }
 
 /// Inserts state from a collection of EIP-1186 proof into an in-memory DB.
 /// The DB can then be used to trace a block, recording state changes as they occur.
-pub fn build_state_from_proofs<T>(block_proofs: T) -> Result<CacheDB<EmptyDB>, StateError>
+pub fn build_state_from_proofs<T>(block_proofs: &T) -> Result<CacheDB<EmptyDB>, StateError>
 where
     T: CompleteAccounts,
 {
     let mut db = CacheDB::new(EmptyDB::default());
 
     for address in block_proofs.addresses() {
-        let info = block_proofs.to_account_info(&address)?;
+        let info = block_proofs.get_account_info(&address)?;
         db.insert_account_info(address, info);
-    }
 
+        let storage = block_proofs.get_account_storage(&address)?;
+        db.replace_account_storage(address, storage)
+            .map_err(|source| StateError::AccountStorageInit {
+                error: source.to_string(),
+                address: hex_encode(address),
+            })?;
+    }
     Ok(db)
 }
