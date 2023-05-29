e580793677728d8729cc03daa9e1dd7f75cf04bd
perama-v <83961755+perama-v@users.noreply.github.com>
2023-05-29 23:14:31 +1000

    feat(inventory): add ssz utils

--- crates/inventory/src/utils.rs
+++ crates/inventory/src/utils.rs
@@ -1,10 +1,15 @@
 use std::{
     array::TryFromSliceError,
     io::{self, Read},
+    num::TryFromIntError,
 };
 
+use ethers::types::{H160, H256, U256, U64};
 use hex::FromHexError;
+use ssz_rs::SimpleSerializeError;
 use thiserror::Error;
+
+use crate::ssz::types::{SszH160, SszH256, SszU256, SszU64};
 
 #[derive(Debug, Error)]
 pub enum UtilsError {
@@ -12,6 +17,10 @@ pub enum UtilsError {
     IoError(#[from] io::Error),
     #[error("Hex utils error {0}")]
     HexUtils(#[from] FromHexError),
+    #[error("SimpleSerialize Error {0}")]
+    SimpleSerializeError(#[from] SimpleSerializeError),
+    #[error("TryFromIntError {0}")]
+    TryFromIntError(#[from] TryFromIntError),
     #[error("TryFromSlice utils error {0}")]
     TryFromSlice(#[from] TryFromSliceError),
 }
@@ -39,4 +48,45 @@ pub fn compress(ssz_bytes: Vec<u8>) -> Result<Vec<u8>, UtilsError> {
     let mut buffer = vec![];
     snap::read::FrameEncoder::new(ssz_bytes.as_slice()).read_to_end(&mut buffer)?;
     Ok(buffer)
+}
+
+/// Convert ethers H256 to SSZ equivalent.
+pub fn h256_to_ssz_h256(input: H256) -> Result<SszH256, UtilsError> {
+    Ok(SszH256::try_from(input.0.to_vec()).map_err(|e| e.1)?)
+}
+
+/// Convert ethers H160 to SSZ equivalent.
+pub fn h160_to_ssz_h160(input: H160) -> Result<SszH160, UtilsError> {
+    Ok(SszH160::try_from(input.0.to_vec()).map_err(|e| e.1)?)
+}
+
+/// Convert ethers U256 to SSZ equivalent.
+///
+/// Output is big endian.
+pub fn u256_to_ssz_u256(input: U256) -> SszU256 {
+    let mut bytes = [0u8; 32];
+    input.to_big_endian(&mut bytes);
+
+    let mut output = SszU256::default();
+    for byte in bytes.into_iter() {
+        output.push(byte)
+    }
+    output
+}
+
+/// Convert ethers U64 to SSZ equivalent.
+///
+/// Output is big endian.
+pub fn u64_to_ssz_u64(input: U64) -> SszU64 {
+    let mut output = SszU64::default();
+    for byte in input.0[0].to_be_bytes() {
+        output.push(byte)
+    }
+    output
+}
+
+/// Converts usize to u16 and prevents overflow.
+pub fn usize_to_u16(input: usize) -> Result<u16, UtilsError> {
+    let num: u16 = input.try_into()?;
+    Ok(num)
 }
