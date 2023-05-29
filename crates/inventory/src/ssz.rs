888e96c02a26b0eac83d78167671bacc54e4b4ca
perama-v <83961755+perama-v@users.noreply.github.com>
2023-05-29 23:15:19 +1000

    fix(inventory): Fix ssz type alias sizes

--- crates/inventory/src/ssz.rs
+++ crates/inventory/src/ssz.rs
@@ -35,10 +35,14 @@ pub mod types {
     use ssz_rs::{List, Vector};
 
     // Variable (uint)
-    pub type SszU64 = List<u8, 64>;
-    pub type SszU256 = List<u8, 256>;
+    /// U64 equivalent
+    pub type SszU64 = List<u8, 8>;
+    /// U256 equivalent
+    pub type SszU256 = List<u8, 32>;
 
     // Fixed (hash, address)
-    pub type SszH256 = Vector<u8, 256>;
-    pub type SszH160 = Vector<u8, 160>;
+    /// H256 Equivalent
+    pub type SszH256 = Vector<u8, 32>;
+    /// H160 Equivalent
+    pub type SszH160 = Vector<u8, 20>;
 }
