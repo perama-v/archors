624fb4d3af2bd6a866c77f6f84f21f8ef7029717
perama-v <83961755+perama-v@users.noreply.github.com>
2023-06-26 12:37:55 +1000

    feat: add blockhash to useage example

--- examples/09_use_proof.rs
+++ examples/09_use_proof.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use archors_inventory::cache::{
-    get_block_from_cache, get_contracts_from_cache, get_proofs_from_cache,
+    get_block_from_cache, get_blockhashes_from_cache, get_contracts_from_cache,
+    get_proofs_from_cache,
 };
 use archors_tracer::{state::BlockProofsBasic, trace::BlockExecutor};
 
@@ -13,16 +14,18 @@ fn main() -> Result<()> {
     // Get state proofs (from peer / disk).
     let required_state = get_proofs_from_cache(block_number)?;
     let required_code = get_contracts_from_cache(block_number)?;
+    let required_blockhashes = get_blockhashes_from_cache(block_number)?;
 
     // Trace block using state (eth_debugTraceTransaction).
     let state = BlockProofsBasic {
         proofs: required_state.proofs,
         code: required_code,
+        block_hashes: required_blockhashes,
     };
     let executor = BlockExecutor::load(block, state)?;
 
     // Either trace the full block or a single transaction of interest.
-    executor.trace_transaction(14)?;
+    executor.trace_transaction(204)?;
     //executor.trace_block()?;
 
     Ok(())
